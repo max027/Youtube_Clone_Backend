b187c540ea27c7df869f11353986c8527434efd3
max027 <saurabhwajage887@gmail.com>
2025-02-25 19:09:14 +0530

    added Utils

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ import dotenv from "dotenv"
 dotenv.config()
 
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
-connectDB()
+connectDB().then((res)=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port:${process.env.PORT}`);
+    })
+}).catch((err)=>{
+    console.log("MONGODO db connection failed:",err);
+    
+})
 
 
