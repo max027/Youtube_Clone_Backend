291563bca8fbc9c6f72c5028b9d7ea58deb020fa
max027 <saurabhwajage887@gmail.com>
2025-03-15 19:05:31 +0530

    added auth

---
+++ src/models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose,{Schema} from "mongoose";
+import bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
+
+const userSchema=new Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+        index:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+        trim:true,
+    },
+    fullname:{
+        type:String,
+        required:true,
+        trim:true,
+        index:true,
+    },
+    avatar:{
+        type:String,
+        required:true,
+    },
+    coverImage:{
+        type:String,
+    },
+    watchHistory:{
+        type:[{
+            type:Schema.Types.ObjectId,
+            ref:"Video",
+        }]
+    },
+    password:{
+        type:String,
+        required:[true ,'Password is required'],
+    },
+    refereshToken:{
+        Type:String
+    }
+},{timestamps:true});
+
+
+userSchema.pre("save",async function(next) {
+    if(!this.isModified("password")){
+        return next()
+    }
+   this.password=bcrypt.hash(this.password,8) 
+   next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+export const User=mongoose.model("User",userSchema)
